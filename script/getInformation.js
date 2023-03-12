05d39b0e03e4c7c955c822ae2a3b9cf76d701e72
Lucas Suota <109081493+LucasSuota@users.noreply.github.com>
2023-03-11 23:23:40 -0300

    monthly or yearly working

--- script/getInformation.js
+++ script/getInformation.js
@@ -1,32 +1,10 @@
-class userInfo{
-    constructor(name, email, phone){
-        this.name = name
-        this.email = email
-        this.phone = phone
-    }
-}
-
-let getUserInfo = () => {
-    const nameId = document.querySelector('#nameId').value
-    const emailId = document.querySelector('#emailId').value
-    const phoneNumberId = document.querySelector('#phoneNumberId').value
-    let person = new userInfo(nameId, emailId, phoneNumberId)
-    return person
-}
-
 document.querySelector('.btn').onclick = () => {
     const hide = document.querySelector('.personalInfo')
     const show = document.querySelector('.planSelection')
-    let person = getUserInfo()
-    if(person.name == '' || person.email == '' || person.phone == ''){
-        alert('You must fill all the blank spaces')
-    } else {
-        console.log(person)
-        hide.style.display = 'none'
-        show.style.display = 'flex'
-    }
-    
+    hide.style.display = 'none'
+    show.style.display = 'flex'
 }
+
 document.querySelector('.btnBack').onclick = () => {
     const hide = document.querySelector('.personalInfo')
     const show = document.querySelector('.planSelection')
@@ -34,3 +12,19 @@ document.querySelector('.btnBack').onclick = () => {
     show.style.display = 'none'
 }
 
+document.querySelector('.checkbox').onclick = () => {
+    let valueList = document.querySelectorAll('.value')
+    if(document.querySelector('.checkbox').checked == true){
+        valueList.forEach(values => {
+            let valuesArray = []
+            valuesArray = (values.innerHTML.replace('$', '').replace('/mo', ''))
+            values.innerHTML = `$${JSON.parse(valuesArray * 10)}/yr`
+        })
+    } else {
+        valueList.forEach(values => {
+            let valuesArray = []
+            valuesArray = values.innerHTML.replace('$', '').replace('/yr', '')
+            values.innerHTML = `$${valuesArray / 10}/mo`
+        })
+    }
+}
