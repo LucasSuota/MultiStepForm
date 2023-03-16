01a235789d2048d29533329e39b77ba502d30d72
Lucas Suota <109081493+LucasSuota@users.noreply.github.com>
2023-03-16 14:31:58 -0300

    next and goback btn add, fix go back

--- script/script.js
+++ script/script.js
@@ -1,30 +1,48 @@
-//next step and previous step buttons
+//next step and previous step
 
-currentTab = 0;
+let currentTab = 0;
 
-function nextStepBtn(n){
+const nextStepBtn = n => {
 
     if(currentTab <= 3){
 
         currentTab = currentTab + n;
-        nextTab = currentTab + n;
+        nextTab = currentTab + 1;
+
+        console.log(currentTab, nextTab)
 
         const currentItem = document.querySelector('.step' + currentTab);
         const nextItem = document.querySelector('.step' + nextTab);
 
-        currentItem.classList.add('d-none')
-        nextItem.classList.remove('d-none')
+        currentItem.classList.add('d-none');
+        nextItem.classList.remove('d-none');
 
     }
 
     if(currentTab > 0){
 
-        const goBackBtn = document.querySelector('.goBackBtn')
+        const goBackBtn = document.querySelector('.goBackBtn');
 
-        goBackBtn.classList.remove('d-none')
+        goBackBtn.classList.remove('d-none');
 
-    }
+    }   
 
-} 
+}
 
+const goBackBtn = n => {
 
+    currentTab = currentTab + n;
+    previousTab = currentTab + 1;
+    nextTab = previousTab + 1;
+
+    console.log(currentTab, previousTab, nextTab)
+
+    const currentItem = document.querySelector('.step' + currentTab);
+    const previousItem = document.querySelector('.step' + previousTab);
+    const nextItem = document.querySelector('.step' + nextTab);
+
+    currentItem.classList.add('d-none');
+    previousItem.classList.remove('d-none');
+    nextItem.classList.add('d-none');
+
+}
