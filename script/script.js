2c55f55e276532deaceaee2db460490ef024872b
Lucas Suota <109081493+LucasSuota@users.noreply.github.com>
2023-03-20 16:31:23 -0300

    steps change color not working

--- script/script.js
+++ script/script.js
@@ -4,13 +4,15 @@ let stepPosition = [1, 2];
 
 const nextStep = (number) => {  
 
+    changeStepBackgroundColor()
+
     const previousTab = document.querySelector('.step' + stepPosition[0]);
     const currentTab = document.querySelector('.step' + stepPosition[1]);
 
     previousTab.classList.add('d-none');
     currentTab.classList.remove('d-none');
 
-    if(stepPosition[0] < 4){
+    if(stepPosition[0] < 3){
 
         stepPosition[0] = stepPosition[0] + number;
         stepPosition[1] = stepPosition[0] + number;
@@ -20,6 +22,8 @@ const nextStep = (number) => {
 };
 
 const previousStep = (number) => {
+
+    changeStepBackgroundColor()
 
     const previousTab = document.querySelector('.step' + stepPosition[0]);
     const currentTab = document.querySelector('.step' + stepPosition[1]);
@@ -35,3 +39,18 @@ const previousStep = (number) => {
     }
 
 };
+
+//changing steps color
+
+const changeStepBackgroundColor = () => {
+    
+    const previousStep = document.querySelector('.step-' + stepPosition[0])
+    const currentStep = document.querySelector('.step-' + stepPosition[1])
+
+    previousStep.style.backgroundColor = 'white'
+    previousStep.style.color = '#174a8b'
+
+    currentStep.style.backgroundColor = 'white'
+    currentStep.style.color = '#174a8b'
+
+}
